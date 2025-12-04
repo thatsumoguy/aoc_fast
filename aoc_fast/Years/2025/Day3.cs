3bd4db4052019bb30974ac2e830ebff9890209cd
Kyle <kylerinne@gmail.com>
2025-12-04 02:01:38 -0500

    cleaned up Day3

--- aoc_fast/Years/2025/Day3.cs
+++ aoc_fast/Years/2025/Day3.cs
@@ -19,38 +19,24 @@ namespace aoc_fast.Years._2025
 
         private static List<byte[]> bytes = [];
 
-        private static ulong Solve(int limit)
+        private static ulong Solve(int limit) => bytes.Select(bank =>
         {
-            try
-            {
-                bytes = [.. input.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(Encoding.UTF8.GetBytes)];
-                var ans = bytes.Select(bank =>
-                {
-                    var max = (byte)0;
-                    var start = 0;
+            var max = (byte)0;
+            var start = 0;
 
-                    return Enumerable.Range(0, limit).Aggregate(0ul, (joltage, digit) =>
-                    {
-                        var end = bank.Length - limit + digit + 1;
-                        (max, start) = Slice.Range(start, end).Aggregate(((byte)0, 0), (ms, i) => bank[i] > ms.Item1 ? (bank[i], i + 1) : ms);
-                        return 10 * joltage + (ulong)(max - (byte)'0');
-                    });
-                    
-                }).Sum();
-                return ans;
-            }
-            catch (Exception ex) { Console.WriteLine(ex); }
-            return 0;
-        }
+            return Enumerable.Range(0, limit).Aggregate(0ul, (joltage, digit) =>
+            {
+                var end = bank.Length - limit + digit + 1;
+                (max, start) = Slice.Range(start, end).Aggregate(((byte)0, 0), (ms, i) => bank[i] > ms.Item1 ? (bank[i], i + 1) : ms);
+                return 10 * joltage + (ulong)(max - (byte)'0');
+            });
+
+        }).Sum();
 
         public static ulong PartOne()
         {
-            try
-            {
-                return Solve(2);
-            }
-            catch (Exception ex) { Console.WriteLine(ex); }
-            return 0;
+            bytes = [.. input.Split("\n", StringSplitOptions.RemoveEmptyEntries).Select(Encoding.UTF8.GetBytes)];
+            return Solve(2);
         }
         public static ulong PartTwo() => Solve(12);
     }
